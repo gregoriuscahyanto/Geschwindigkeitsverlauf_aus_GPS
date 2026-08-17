1bc37bed484e92dc37500c6cd30f7b8bbf5c055d
Gregorius Cahyanto <153749215+gregoriuscahyanto@users.noreply.github.com>
2026-08-17 10:34:12 +0200

    Install corrected curve radius threshold policy

--- qt_route_selector/__init__.py
+++ qt_route_selector/__init__.py
@@ -1,8 +1,11 @@
 """Qt-based local route selection and offline routing package."""
 
 from . import speed_simulation as _speed_simulation
+from .curve_radius_policy import install_curve_radius_threshold as _install_curve_radius_threshold
 from .driver_presets import install_complete_driver_profiles as _install_complete_driver_profiles
 
 _install_complete_driver_profiles(_speed_simulation)
+_install_curve_radius_threshold(_speed_simulation)
 
 del _install_complete_driver_profiles
+del _install_curve_radius_threshold
