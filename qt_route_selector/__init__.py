173d02cebf2ab495e62c58d4b39a4731188d2727
Gregorius Cahyanto <153749215+gregoriuscahyanto@users.noreply.github.com>
2026-08-18 10:18:58 +0200

    Enable segment-based routing snaps

--- qt_route_selector/__init__.py
+++ qt_route_selector/__init__.py
@@ -1,11 +1,15 @@
 """Qt-based local route selection and offline routing package."""
 
+from . import local_router as _local_router
 from . import speed_simulation as _speed_simulation
 from .curve_radius_policy import install_curve_radius_threshold as _install_curve_radius_threshold
 from .driver_presets import install_complete_driver_profiles as _install_complete_driver_profiles
+from .segment_snapping import install_segment_snapping as _install_segment_snapping
 
 _install_complete_driver_profiles(_speed_simulation)
 _install_curve_radius_threshold(_speed_simulation)
+_install_segment_snapping(_local_router)
 
 del _install_complete_driver_profiles
 del _install_curve_radius_threshold
+del _install_segment_snapping
